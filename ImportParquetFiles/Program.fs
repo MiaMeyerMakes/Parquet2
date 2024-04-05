14acf29811e43205e54c88f45899a9951c8a8982
Mia.Meyer <miameyer15@gmail.com>
2024-04-05 14:33:06 +0200

    Write data to Parquet file

--- ImportParquetFiles/Program.fs
+++ ImportParquetFiles/Program.fs
@@ -1,3 +1,32 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
-// testingtesting
+﻿open Parquet
+open Parquet.Data
+open Parquet.Schema
+
+open Parquet
+
+// Create file schema
+let schema =
+    ParquetSchema(
+        DataField<int>("id"),
+        DataField<string>("city")
+    )
+
+// Create data columns with schema metadata and the data you need
+let idColumn =
+    DataColumn(
+        schema.DataFields.[0],
+        [| 1; 2 |]
+    )
+    
+let cityColumn =
+    DataColumn(
+        schema.DataFields.[1],
+        [| "London"; "Derby" |]
+    )
+
+// Create a new Parquet file
+
+let myPath = "C:/Users/Mia.Meyer/Parquet Files/test.parquet"
+// let fileStream = System.IO.File.OpenWrite(myPath)
+
+
